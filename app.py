d4926fde0016f2194fc32eed1bd3a1fc3c9cc731
Banjo Donila <banjo@jumphost.lab.integrate.zone>
2021-09-27 17:08:01 +1300

    Updates

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/hello/<name>')
 def hello_name(name):
-return 'I am Robert B! Nice to meet you %s!' % name
+  return 'I am Robert B! Nice to meet you %s!' % name
 
 if __name__ == '__main__':
-app.run(host="0.0.0.0", port=8080)
+  app.run(host="0.0.0.0", port=8080)
 
